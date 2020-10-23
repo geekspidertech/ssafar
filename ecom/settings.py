f8fc2ab743e1144c644a58ffa1b9f3e2ba5b083e
Rohit Sinha <rohitsinha@Rohits-MacBook-Pro.local>
2020-10-24 00:06:47 +0530

    Updated settings for heroku

--- ecom/settings.py
+++ ecom/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from oscar.defaults import *
-
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -272,3 +272,4 @@ OSCAR_DASHBOARD_NAVIGATION.append({
 'icon': 'icon-globe',
 
 })
+django_heroku.settings(locals())
